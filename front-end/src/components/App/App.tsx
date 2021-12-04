989925a66c44624632d01e9eb29ed0a4d8311ccb
Arthur <arthur@corenzan.com>
2021-12-04 18:14:27 -0300

    Wip

--- front-end/src/components/App/App.tsx
+++ front-end/src/components/App/App.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import { useLocation, useRoute } from "wouter";
+import { nanoid } from "nanoid";
+
 import { Layout } from "src/components/Layout";
 import { Header } from "src/components/Header";
 import { Content } from "src/components/Content";
@@ -5,7 +9,24 @@ import { Deck } from "src/components/Deck";
 
 import styles from "./App.module.css";
 
+const createRoomId = () => {
+  return nanoid(10);
+};
+
 export const App = () => {
+  const [match, params] = useRoute("/:roomId");
+  const [, setLocation] = useLocation();
+
+  const { roomId } = params ?? {};
+
+  useEffect(() => {
+    if (!match) {
+      setLocation("/" + createRoomId(), { replace: true });
+    }
+  }, [match, setLocation]);
+
+  console.log({ roomId });
+
   return (
     <Layout>
       <Header />
