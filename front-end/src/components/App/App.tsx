e7caea0cf447428361583837a1529ee395cf5812
Arthur <arthur@corenzan.com>
2021-12-04 15:03:27 -0300

    Initial setup

---
+++ front-end/src/components/App/App.tsx
@@ -0,0 +1,5 @@
+import "./App.module.css";
+
+export const App = () => {
+  return <div className="App">Hello, Twitch</div>;
+};
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "src/index.css";
+import { App } from "src/components/App";
+import reportWebVitals from "src/reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
