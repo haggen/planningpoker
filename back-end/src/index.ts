ed8ff6d3d9fc32fcbb6bb0496b2b026f56e4e491
Arthur <arthur@corenzan.com>
2021-12-05 17:05:00 -0300

    WebSocket server working

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,8 +1,32 @@
 import { createServer } from "http";
+import { Socket } from "net";
+import { URL } from "url";
+import { WebSocketServer } from "ws";
 
-const srv = createServer((req, resp) => {
-  resp.writeHead(200, { "Content-Type": "text/plain" });
-  resp.end("Hello World");
+const webSocketServer = new WebSocketServer({ noServer: true });
+
+const webServer = createServer((req, resp) => {
+  // resp.end();
 });
 
-srv.listen(parseInt(process.env.PORT ?? "3000", 10));
+webServer.on("upgrade", (req, socket, head) => {
+  webSocketServer.handleUpgrade(req, socket as Socket, head, (ws) => {
+    webSocketServer.emit("connection", ws, req);
+  });
+});
+
+webSocketServer.on("connection", (ws, req) => {
+  const { pathname } = new URL(req.url ?? "/", "http://localhost/");
+
+  console.log(`client connected from channel ${pathname}`);
+
+  ws.on("message", (message) => {
+    console.log(`message received in channel ${pathname}: ${message}`);
+  });
+
+  ws.on("close", (code) => {
+    console.log(`client disconnected from channel ${pathname}`);
+  });
+});
+
+webServer.listen(parseInt(process.env.PORT ?? "5000", 10));
