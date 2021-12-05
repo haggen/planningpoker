33ecd010b6140736a491211ebe1bf0a1bb51af0e
Arthur <arthur@corenzan.com>
2021-12-05 16:18:34 -0300

    Fix web server in TypeScript

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import { createServer } from "http";
+
+const srv = createServer((req, resp) => {
+  resp.writeHead(200, { "Content-Type": "text/plain" });
+  resp.end("Hello World");
+});
+
+srv.listen(parseInt(process.env.PORT ?? "3000", 10));
