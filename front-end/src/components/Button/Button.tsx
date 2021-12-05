5edccf9f0f3e23bbd968ceaa7a47c7151688f0e0
Arthur <arthur@corenzan.com>
2021-12-05 11:07:29 -0300

    Add onClick handler

--- front-end/src/components/Button/Button.tsx
+++ front-end/src/components/Button/Button.tsx
@@ -1,14 +1,23 @@
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 import c from "classnames";
 
 import styles from "./Button.module.css";
 
 type ButtonProps = {
   type?: "default" | "ghost";
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   children: ReactNode;
 };
 
-export const Button = ({ type = "default", children }: ButtonProps) => {
+export const Button = ({
+  type = "default",
+  onClick,
+  children,
+}: ButtonProps) => {
   const className = c(styles.button, styles[type]);
-  return <button className={className}>{children}</button>;
+  return (
+    <button className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
